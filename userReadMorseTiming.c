8b3a14a166482e27d93833902468f7438228a3a2
Uy Nguyen <nguyenuy@gmail.com>
2014-10-23 19:31:30 -0400

    Update

--- userReadMorseTiming.c
+++ userReadMorseTiming.c
@@ -247,7 +247,7 @@ int main(void)
       end = clock();
       cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
       printf("cpu_time_used:%f", cpu_time_used);
-      return
+      return;
     }
   }
         
