a82a75e477d1acd445358b78037942c1bc79cb9a
Uy Nguyen <nguyenuy@gmail.com>
2014-10-30 17:13:48 -0400

    Update

--- kernel_mods/morseLED.c
+++ kernel_mods/morseLED.c
@@ -215,15 +215,13 @@ device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
   //printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
   //return -EINVAL;
-  size_t copy_size = len*sizeof(char)*27;
+  //size_t copy_size = len*sizeof(char)*27;
   //copy_from_user(english_msg, buff, copy_size);
-  printk(KERN_INFO "Hi");
+  //printk(KERN_INFO "Hi");
   //string_to_morse(english_msg, copy_size);
   //printk(KERN_INFO "Translated Message: %s\n", english_msg);
-
-
-  
-  return 0;
+  printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
+  return -EINVAL;
 }
 
 /*******************************
