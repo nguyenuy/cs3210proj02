df01b80b7a81cccc17d5e0628725077df4851497
Uy Nguyen <nguyenuy@gmail.com>
2014-10-30 12:12:40 -0400

    Test

--- kernel_mods/gpiomod.c
+++ kernel_mods/gpiomod.c
@@ -1,81 +1,159 @@
+/*
+ * Basic Linux Kernel module using GPIO interrupts.
+ *
+ * Author:
+ * 	Stefan Wendler (devnull@kaltpost.de)
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
+#include <linux/interrupt.h> 
 
-/*
- *  GPIO Definitions 
- */
-#define GP_LED                 (3) // GPIO3 is GP LED - LED connected between Cypress CY8C9540A and RTC battery header
-#define GP_5                   (17) //GPIO17 corresponds to Arduino PIN5
-#define GP_6                   (24) //GPIO24 corresponds to Arduino PIN6
-#define GP_7                   (27) //GPIO27 corresponds to Arduino PIN7
-
-#define SUCCESS 0
-#define DEVICE_NAME "chardev"	/* Dev name as it appears in /proc/devices   */
-#define BUF_LEN 80		/* Max length of the message from the device */
-
- /* 
- * Global variables are declared as static, so are global within the file. 
- */
-
-static int Major;		/* Major number assigned to our device driver */
-static int Device_Open = 0;	/* Is device open?  
-				 * Used to prevent multiple access to device */
-static char msg[BUF_LEN];	/* The msg the device will give when asked */
-static char *msg_Ptr;
-
-/*
- * Struct defining pins, direction and inital state 
- */
+/* Define GPIOs for LEDs */
 static struct gpio leds[] = {
-		{  GP_LED, GPIOF_OUT_INIT_HIGH, "LED 1" },
+		{  3, GPIOF_OUT_INIT_LOW, "LED 1" },
 };
+
+/* Define GPIOs for BUTTONS */
+static struct gpio buttons[] = {
+		{ 17, GPIOF_IN, "BUTTON 1" },	// turns LED on
+		{ 24, GPIOF_IN, "BUTTON 2" },	// turns LED off
+};
+
+/* Later on, the assigned IRQ numbers for the buttons are stored here */
+static int button_irqs[] = { -1, -1 };
+
+/*
+ * The interrupt service routine called on button presses
+ */
+static irqreturn_t button_isr(int irq, void *data)
+{
+	if(irq == button_irqs[0] && !gpio_get_value(leds[0].gpio)) {
+			gpio_set_value(leds[0].gpio, 1);
+	}
+	else if(irq == button_irqs[1] && gpio_get_value(leds[0].gpio)) {
+			gpio_set_value(leds[0].gpio, 0);
+	}
+
+	return IRQ_HANDLED;
+}
 
 /*
  * Module init function
  */
-static int __init gpiomod_init(void)
+static int __init gpiomode_init(void)
 {
-	Major = register_chrdev(0, DEVICE_NAME, &fops);
-	if (Major < 0) {
-	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-	  return Major;
-	}
-	
 	int ret = 0;
 
 	printk(KERN_INFO "%s\n", __func__);
 
-	// register LED GPIOs, turn LEDs on
+	// register LED gpios
 	ret = gpio_request_array(leds, ARRAY_SIZE(leds));
 
 	if (ret) {
-		printk(KERN_ERR "Unable to request GPIOs: %d\n", ret);
+		printk(KERN_ERR "Unable to request GPIOs for LEDs: %d\n", ret);
+		return ret;
 	}
-	gpio_set_value(leds[0].gpio, 1);
+	
+	// register BUTTON gpios
+	ret = gpio_request_array(buttons, ARRAY_SIZE(buttons));
 
-	return ret;
+	if (ret) {
+		printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
+		goto fail1;
+	}
+
+	printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
+	
+	ret = gpio_to_irq(buttons[0].gpio);
+
+	if(ret < 0) {
+		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
+		goto fail2;
+	}
+
+	button_irqs[0] = ret;
+
+	printk(KERN_INFO "Successfully requested BUTTON1 IRQ # %d\n", button_irqs[0]);
+
+	ret = request_irq(button_irqs[0], button_isr, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#button1", NULL);
+
+	if(ret) {
+		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
+		goto fail2;
+	}
+
+
+	ret = gpio_to_irq(buttons[1].gpio);
+
+	if(ret < 0) {
+		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
+		goto fail2;
+	}
+		
+	button_irqs[1] = ret;
+
+	printk(KERN_INFO "Successfully requested BUTTON2 IRQ # %d\n", button_irqs[1]);
+
+	ret = request_irq(button_irqs[1], button_isr, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#button2", NULL);
+
+	if(ret) {
+		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
+		goto fail3;
+	}
+
+	return 0;
+
+// cleanup what has been setup so far
+fail3:
+	free_irq(button_irqs[0], NULL);
+
+fail2: 
+	gpio_free_array(buttons, ARRAY_SIZE(leds));
+
+fail1:
+	gpio_free_array(leds, ARRAY_SIZE(leds));
+
+	return ret;	
 }
 
-/*
+/**
  * Module exit function
  */
-static void __exit gpiomod_exit(void)
+static void __exit gpiomode_exit(void)
 {
 	int i;
 
 	printk(KERN_INFO "%s\n", __func__);
-	unregister_chrdev(Major, DEVICE_NAME);
+
+	// free irqs
+	free_irq(button_irqs[0], NULL);
+	free_irq(button_irqs[1], NULL);
+	
 	// turn all LEDs off
 	for(i = 0; i < ARRAY_SIZE(leds); i++) {
 		gpio_set_value(leds[i].gpio, 0); 
 	}
 	
-	// unregister all GPIOs
+	// unregister
 	gpio_free_array(leds, ARRAY_SIZE(leds));
+	gpio_free_array(buttons, ARRAY_SIZE(buttons));
 }
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Stefan Wendler");
+MODULE_DESCRIPTION("Basic Linux Kernel module using GPIO interrupts");
 
-module_init(gpiomod_init);
-module_exit(gpiomod_exit);
+module_init(gpiomode_init);
+module_exit(gpiomode_exit);
