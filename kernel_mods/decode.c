b97daad138f58db931b1b827d9ca707fdde18411
cyang72 <user@2200-vm.2200-vm>
2014-10-30 21:51:09 -0400

    fix

--- kernel_mods/decode.c
+++ kernel_mods/decode.c
@@ -36,7 +36,7 @@ static struct gpio switch_gpio[] = {
 
 typedef __u16 morse_t;
 int len,temp;
-morse_t charToBin[CHAR_SIZE];
+
 
 /* 
  * Global variables are declared as static, so are global within the file. 
@@ -61,73 +61,62 @@ int getDecodeKey(int* buf,int n){
 }
 
 /* *******************************/
-void initialize_char_to_bin_array() {
-  int i=0;
-  for(; i<CHAR_SIZE;i++){
-    charToBin[i] = 0;
-  }
-  charToBin['a'] = 0b110;
-  charToBin['b'] = 0b10111;
-  charToBin['c'] = 0b10101;
-  charToBin['d'] = 0b1011;
-  charToBin['e'] = 0b11;
-  charToBin['f'] = 0b11101;
-  charToBin['g'] = 0b1001;
-  charToBin['h'] = 0b11111;
-  charToBin['i'] = 0b111;
-  charToBin['j'] = 0b11000;
-  charToBin['k'] = 0b1010;
-  charToBin['l'] = 0b11011;
-  charToBin['m'] = 0b100;
-  charToBin['n'] = 0b101;
-  charToBin['o'] = 0b1000;
-  charToBin['p'] = 0b11001;
-  charToBin['q'] = 0b10010;
-  charToBin['r'] = 0b1101;
-  charToBin['s'] = 0b1111;
-  charToBin['t'] = 0b10;
-  charToBin['u'] = 0b1110;
-  charToBin['v'] = 0b11110;
-  charToBin['w'] = 0b1100;
-  charToBin['x'] = 0b10110;
-  charToBin['y'] = 0b10100;
-  charToBin['z'] = 0b10011;
-  charToBin['1'] = 0b110000;
-  charToBin['2'] = 0b111000;
-  charToBin['3'] = 0b111100;
-  charToBin['4'] = 0b111110;
-  charToBin['5'] = 0b111111;
-  charToBin['6'] = 0b101111;
-  charToBin['7'] = 0b100111;
-  charToBin['8'] = 0b100011;
-  charToBin['9'] = 0b100001;
-  charToBin['0'] = 0b100000;
-  charToBin['.'] = 0b1101010;
-  charToBin['?'] = 0b1110011;
-  charToBin['!'] = 0b1010100;
-  charToBin['('] = 0b101001;
-  charToBin[')'] = 0b1010011;
-  charToBin[':'] = 0b1000111;
-  charToBin['='] = 0b101110;
-  charToBin['-'] = 0b1011110;
-  charToBin['"'] = 0b1101101;
-  charToBin[','] = 0b1001100;
-  charToBin['\'']= 0b1100001;
-  charToBin['/'] = 0b101101;
-  charToBin[';'] = 0b1010101;
-  charToBin['_'] = 0b1110010;
-  charToBin['@'] = 0b1100101;
-  charToBin[' '] = 0b11111111;
-  
-  char diff = 'A'-'a';
-  char ch = 'A';
-  for(; ch<='Z'; ch++){
-    char lower = ch - diff;
-    charToBin[ch] = charToBin[lower];
-  }   
+void initMorseMap(char* binToChar){
+  binToChar[0b110] = 'a';
+  binToChar[0b10111] = 'b';
+  binToChar[0b10101] = 'c';
+  binToChar[0b1011] = 'd';
+  binToChar[0b11] = 'e';
+  binToChar[0b11101] = 'f';
+  binToChar[0b1001] = 'g';
+  binToChar[0b11111] = 'h';
+  binToChar[0b111] = 'i';
+  binToChar[0b11000] = 'j';
+  binToChar[0b1010] = 'k';
+  binToChar[0b11011] = 'l';
+  binToChar[0b100] = 'm';
+  binToChar[0b101] = 'n';
+  binToChar[0b1000] = 'o';
+  binToChar[0b11001] = 'p';
+  binToChar[0b10010] = 'q';
+  binToChar[0b1101] = 'r';
+  binToChar[0b1111] = 's';
+  binToChar[0b10] = 't';
+  binToChar[0b1110] = 'u';
+  binToChar[0b11110] = 'v';
+  binToChar[0b1100] = 'w';
+  binToChar[0b10110] = 'x';
+  binToChar[0b10100] = 'y';
+  binToChar[0b10011] = 'z';
+  binToChar[0b110000] = '1';
+  binToChar[0b111000] = '2';
+  binToChar[0b111100] = '3';
+  binToChar[0b111110] = '4';
+  binToChar[0b111111] = '5';
+  binToChar[0b101111] = '6';
+  binToChar[0b100111] = '7';
+  binToChar[0b100011] = '8';
+  binToChar[0b100001] = '9';
+  binToChar[0b100000] = '0';
+  binToChar[0b1101010] = '.';
+  binToChar[0b1110011] = '?';
+  binToChar[0b1010100] = '!';
+  binToChar[0b101001] = '(';
+  binToChar[0b1010011] = ')';
+  binToChar[0b1000111] = ':';
+  binToChar[0b101110] = '=';
+  binToChar[0b1011110] = '-';
+  binToChar[0b1101101] = '"';
+  binToChar[0b1001100] = ',';
+  binToChar[0b1100001] = '\'';
+  binToChar[0b101101] = '/';
+  binToChar[0b1010101] = ';';
+  binToChar[0b1110010] = '_';
+  binToChar[0b1100101] = '@';
+  binToChar[0b11111111]= ' ';
 }
 
-void init(int* A, int n){
+void init(char* A, int n){
   int i=0;
   for(; i<n; i++){
     A[i] = -2;
@@ -137,14 +126,15 @@ void init(int* A, int n){
 static int __init morse_init(void)
 {
   printk(KERN_INFO "%s\n", __func__);
-   
+  
   printk(KERN_INFO "the driver, create a dev file with\n");
   printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
   printk(KERN_INFO "the device file.\n");
   printk(KERN_INFO "Remove the device file and module when done.\n");
   gpio_set_value(morse_gpio[0].gpio, 1);
 
-  initialize_char_to_bin_array();
+  char morseMap[512];
+  initMorseMap(morseMap);
   
   int BUF_LEN = 16;
   int LONG_WAIT = 100;
@@ -179,7 +169,7 @@ static int __init morse_init(void)
 	}
       }
       int key = getDecodeKey(buf, BUF_LEN);
-      char result = charToBin[key];
+      char result = morseMap[key];
       printk(KERN_INFO "do you mean %c?\n", result);
       break;
     }    
